0e7c25b35331ea0f986ed286ed7cae5656a50d6c
Sasha <vdovicenco.alexandr@gmail.com>
2018-04-29 11:02:11 +0300

    Added SprayARt Project to Origami

--- app/src/main/java/com/koshka/origami/fragments/main/origami/OrigamiFragment.java
+++ app/src/main/java/com/koshka/origami/fragments/main/origami/OrigamiFragment.java
@@ -1,20 +1,25 @@
 package com.koshka.origami.fragments.main.origami;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.sprayart.SprayArt;
 import com.koshka.origami.R;
 import com.koshka.origami.helpers.fragment.OrigamiFragmentHelper;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by imuntean on 7/20/16.
@@ -25,8 +30,8 @@ public class OrigamiFragment extends Fragment {
 
     @BindView(R.id.origami_sliding_layout)
     SlidingUpPanelLayout slidingUpPanelLayout;
-    @BindView(R.id.origami_create_button)
-    TextView origamiText;
+    @BindView(R.id.origamiImage)
+    ImageView origamiImage;
 
     private OrigamiFragmentHelper fragmentHelper;
 
@@ -44,9 +49,11 @@ public class OrigamiFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         fragmentHelper = new OrigamiFragmentHelper(getActivity(), slidingUpPanelLayout);
-        Typeface font = Typeface.createFromAsset(getActivity().getAssets(),"fonts/actonia.ttf");
-        origamiText.setTypeface(font);
-
+        origamiImage.setOnClickListener(view1 -> {
+            Log.e("OrigamiImage","click");
+            Intent intent = new Intent(getActivity(), SprayArt.class);
+            startActivity(intent);
+        });
     }
 
 
---
+++ sprayart/src/main/java/com/example/sprayart/SprayArt.kt
@@ -0,0 +1,135 @@
+package com.example.sprayart
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.ViewGroup
+import android.view.WindowManager
+import cn.easyar.Engine
+import android.provider.MediaStore
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.content.FileProvider
+import android.view.View
+import kotlinx.android.synthetic.main.activity_spray_art.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class SprayArt: AppCompatActivity() {
+    companion object {
+        private val key = "wcC0IXh6b9hMnX4Ol9tj7eLoZQpTRyodekJBJ990aSpqmlnCwU9ufzqYxzWgnTswM3b6UTpo3oasgjdsMiM33adGg4M0p2AQWbM3LT70MnQwNlLbRLr1nA8e66dBT6t0Dsd8VQFXCDLNfdGZf1JwmHiMoi7S4wivaXAcuoS0KCmD3iMKcAwOo4COR9PDGXLrEUjByuUx\n"
+    }
+
+    private var glView: GLView? = null
+    private val REQUEST_IMAGE_CAPTURE = 100
+    private val REQUEST_SELECT_PICTURE = 101
+    private var mCurrentPhotoPath = ""
+    private val helper by lazy { TextureHelper(BitmapFactory.decodeResource(resources, R.drawable.sausages)) }
+    private val photosMap = mutableMapOf<String, String>()
+    private var selectedGrafitti : Uri = Uri.EMPTY
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spray_art)
+        window.setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        if (!Engine.initialize(this, key)) {
+            Log.e("HelloAR", "Initialization Failed.")
+        }
+
+        photosMap["${R.drawable.sausages}"] = "idback.jpg"
+        glView = GLView(this, photosMap)
+        preview.addView(glView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+
+        addWallBtn.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+        addGrafityBtn.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_SELECT_PICTURE)
+        }
+        plusBtn.setOnClickListener{
+
+        }
+        minusBtn.setOnClickListener {
+
+        }
+        doneBtn.setOnClickListener {
+            plusBtn.visibility = View.GONE
+            minusBtn.visibility = View.GONE
+            doneBtn.visibility = View.GONE
+            imageView.visibility = View.GONE
+            photosMap[selectedGrafitti.toString()] = mCurrentPhotoPath
+            reloadGlView()
+            //save data to server here
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preview.removeView(glView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (glView!!.parent == null)
+            preview.addView(glView)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == FragmentActivity.RESULT_OK)
+        {
+            imageView.visibility = View.VISIBLE
+            imageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath))
+
+        } else if(requestCode == REQUEST_SELECT_PICTURE && resultCode == RESULT_OK) {
+            plusBtn.visibility = View.VISIBLE
+            minusBtn.visibility = View.VISIBLE
+            doneBtn.visibility = View.VISIBLE
+            selectedGrafitti = data!!.data
+
+
+        }
+    }
+
+    private fun reloadGlView()
+    {
+        preview.removeView(glView)
+        glView = GLView(this, photosMap)
+        preview.addView(glView)
+    }
+
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir      /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.absolutePath
+        return image
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            val photoFile : File = createImageFile()
+            val photoURI = FileProvider.getUriForFile(this@SprayArt, "com.example.sprayart.fileprovider", photoFile)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+}
